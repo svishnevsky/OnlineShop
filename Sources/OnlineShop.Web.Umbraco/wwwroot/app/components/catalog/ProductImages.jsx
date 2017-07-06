7be1d724337cee8e6ee08941847f159292d623f2
Sergey Vishnevsky <svishnevskij@gmail.com>
2017-07-06 13:43:50 +0300

    Fixed zoom elevate

--- Sources/OnlineShop.Web.Umbraco/wwwroot/app/components/catalog/ProductImages.jsx
+++ Sources/OnlineShop.Web.Umbraco/wwwroot/app/components/catalog/ProductImages.jsx
@@ -6,7 +6,7 @@ import '../../../../scripts/jquery.elevateZoom-3.0.3.min.js'
 
 const defaults = {
     gallery: 'gallery_01',
-    zoomType: "inner",
+    zoomType: 'inner',
     cursor: 'crosshair',
     galleryActiveClass: 'active',
     imageCrossfade: true,
@@ -15,25 +15,19 @@ const defaults = {
 
 export default class ProductImages extends Component {
     componentDidMount() {
-        $(ReactDOM.findDOMNode(this)).find('.headPhoto').elevateZoom(defaults);
+        $(ReactDOM.findDOMNode(this)).find('#mainImage').elevateZoom(defaults);
     }
 
     render() {
         return (
             <div className='product_img'>
-                <div className='zoomWrapper' style={{ height: '330px', width: '330px' }}>
-                    <img src={getCropUrl(this.props.images[0].url, 330)} className='headPhoto' data-zoom-image={this.props.images[0].url} alt={this.props.images[0].name} />
-                </div>
-                <div id='gallery_01'>
-                    <div className='thumbs'>
-                        <div className='thumbs_w'>
-                            {this.props.images.map(img =>
-                                <a key={img.url} data-image={getCropUrl(img.url, 330)} data-zoom-image={img.url}>
-                                    <img id={`img_${img.name}`} className='thumb_fix' src={getCropUrl(img.url, 60)} />
-                                </a>)
-                            }
-                        </div>
-                    </div>
+                <img id='mainImage' src={getCropUrl(this.props.images[0].url, 330)} className='headPhoto' data-zoom-image={this.props.images[0].url} alt={this.props.images[0].name} />
+                <div id='gallery_01' className='thumbs'>
+                    {this.props.images.map((img, index) =>
+                        <a key={index} data-image={getCropUrl(img.url, 330)} data-zoom-image={img.url}>
+                            <img id={`img_${index}`} className='thumb_fix' src={getCropUrl(img.url, 60)} />
+                        </a>)
+                    }
                 </div>
             </div>
         );
