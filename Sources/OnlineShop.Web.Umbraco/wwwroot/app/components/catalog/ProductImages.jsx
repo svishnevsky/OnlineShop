3943b5a0524c14f18cd8e56e456527d44d9f6508
Sergey Vishnevsky <svishnevskij@gmail.com>
2017-07-06 13:19:16 +0300

    Added eveleteZoom on product images

--- Sources/OnlineShop.Web.Umbraco/wwwroot/app/components/catalog/ProductImages.jsx
+++ Sources/OnlineShop.Web.Umbraco/wwwroot/app/components/catalog/ProductImages.jsx
@@ -1,18 +1,34 @@
 ﻿import React, { Component } from 'react'
+import ReactDOM from 'react-dom'
 import { getCropUrl } from '../../utils/common'
+import $ from 'jquery'
+import '../../../../scripts/jquery.elevateZoom-3.0.3.min.js'
+
+const defaults = {
+    gallery: 'gallery_01',
+    zoomType: "inner",
+    cursor: 'crosshair',
+    galleryActiveClass: 'active',
+    imageCrossfade: true,
+    loadingIcon: '/images/spinner.gif'
+}
 
 export default class ProductImages extends Component {
+    componentDidMount() {
+        $(ReactDOM.findDOMNode(this)).find('.headPhoto').elevateZoom(defaults);
+    }
+
     render() {
         return (
             <div className='product_img'>
-                <div className='zoomWrapper'>
-                    <img id='img_main' src={getCropUrl(this.props.images[0].url, 450)} className='headPhoto' data-zoom-image={this.props.images[0].url} alt={this.props.images[0].name} />
+                <div className='zoomWrapper' style={{ height: '330px', width: '330px' }}>
+                    <img src={getCropUrl(this.props.images[0].url, 330)} className='headPhoto' data-zoom-image={this.props.images[0].url} alt={this.props.images[0].name} />
                 </div>
                 <div id='gallery_01'>
                     <div className='thumbs'>
                         <div className='thumbs_w'>
                             {this.props.images.map(img =>
-                                <a key={img.url} data-image={getCropUrl(img.url, 450)} data-zoom-image={img.url}>
+                                <a key={img.url} data-image={getCropUrl(img.url, 330)} data-zoom-image={img.url}>
                                     <img id={`img_${img.name}`} className='thumb_fix' src={getCropUrl(img.url, 60)} />
                                 </a>)
                             }
